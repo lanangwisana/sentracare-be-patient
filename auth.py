db0dab3f6e0883fada49abf36a033913d31ee7dc
lanangwisana <lanangwisana@gmail.com>
2025-12-26 16:30:16 +0700

    update code from auth

--- auth.py
+++ auth.py
@@ -20,7 +20,7 @@ def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(securit
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token tidak valid")
     return payload
 
-def require_roles(allowed_roles: list):
+def require_role(allowed_roles: list):
     def _inner(user=Depends(get_current_user)):
         if user.get("role") not in allowed_roles:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Akses ditolak")
